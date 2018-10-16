80bdc804d85cc6c58f542993bac337ac33133053
Ravi Gupta <ravigupta112>
2018-10-16 15:05:45 +0530

    falsh message

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Todo;
-
+use Session;
 class TodosController extends Controller
 {
 
@@ -21,7 +21,7 @@ class TodosController extends Controller
         $todoModel = new Todo();
         $todoModel->todo = $request->todo;
         $todoModel->save();
-
+        Session::flash('success', "todo crerated");
         return redirect()->back();
     }
 
@@ -29,7 +29,7 @@ class TodosController extends Controller
     {
         $todo = Todo::find($id);
         $todo->delete();
-
+        Session::flash('success', "todo deleted");
         return redirect()->back();
     }
 
@@ -46,6 +46,8 @@ class TodosController extends Controller
         $todo = Todo::find($id);
         $todo->completed = 1;
         $todo->save();
+        
+        Session::flash('success', "todo mark as completed");
         return redirect()->back();
     }
 
@@ -55,7 +57,7 @@ class TodosController extends Controller
         $todo->todo = $request->todo;
         $todo->save();
 
-
+        Session::flash('success', "todo updated");
         return redirect()->route('todos');
     }
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -72,6 +72,11 @@
         </style>
     </head>
     <body>
+        @if(Session::has('success'))
+            <div class="alert alert-success" role="alert">
+                    {{ Session::get('success') }}
+            </div>
+        @endif
         <div class="flex-center position-ref full-height">
 
 
